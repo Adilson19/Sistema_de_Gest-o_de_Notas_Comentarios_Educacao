a56503c454f7c7b571abbd95f9118b2e74e6c3ed
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-08-25 09:42:06 +0100

    feat: criando as migrations de alunos

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('alunos', AlunoController::class);
+Route::resource('professores', ProfessorController::class);
